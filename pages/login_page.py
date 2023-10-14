e785ff0342702182a9349a0b455e763cd71ea9d9
Emilia Buczek <101749808+Ebmili@users.noreply.github.com>
2023-10-14 22:42:16 +0200

    feat: added first test for the LogInPage, added test for LogIn

--- pages/login_page.py
+++ pages/login_page.py
@@ -1,24 +1,20 @@
-First_automatic_test,_asserts
 import time
 
-main
 from pages.base_page import BasePage
 
 
 class LoginPage(BasePage):
     login_field_xpath = "//*[@id='login']"
- First_automatic_test,_asserts
     password_field_xpath = "//*[@id='password']"
-    sign_in_button_xpath = "//button[starts-with(@class,'MuiButtonBase-root')]"
-    remind_password_xpath = "//a[starts-with(@class,'MuiTypography-root')]"
-    language_selection_xpath = "/html/body/div/form/div/div[2]/div/input"
-    title_scouts_panel_xpath = "//h5[starts-with(@class, 'MuiTypography-root')]"
-    title_of_box_xpath = "//*[contains(@class, 'MuiTypography-root MuiTypography-h5')]"
-    header_of_box = 'Scouts Panel'
-    add_a_player_button_xpath = "//*[@id='__next']/div[1]/main/div[3]/div[2]/div/div/a/button/span[1]"
-
-    login_url = "https://scouts-test.futbolkolektyw.pl/en"
-    expected_title = 'Scouts panel - sign in'
+    sign_in_button_xpath = "//button/span[1]"
+    login_url = 'https://scouts.futbolkolektyw.pl/en//login'
+    polish_login_url = 'https://scouts.futbolkolektyw.pl/en//pl/login'
+    expected_title = "Scouts panel - sign in"
+    expected_polish_title = "Scouts panel - zaloguj"
+    header_of_box_xpath = "//*[@id='__next']/form/div/div[1]/h5"
+    expected_header_of_box = 'Scouts Panel'
+    english_language_listbox_xpath = "//div[2]/div/div"
+    polski_option_xpath = "//div[3]/ul/li[1]"
 
     def type_in_email(self, email):
         self.field_send_keys(self.login_field_xpath, email)
@@ -26,19 +22,23 @@ class LoginPage(BasePage):
     def type_in_password(self, password):
         self.field_send_keys(self.password_field_xpath, password)
 
-    def click_on_the_sign_in_button(self):
+    def click_sign_in_button(self):
         self.click_on_the_element(self.sign_in_button_xpath)
 
     def title_of_page(self):
-        time.sleep(5)
+        self.wait_for_element_to_be_clickable(self.sign_in_button_xpath)
         assert self.get_page_title(self.login_url) == self.expected_title
 
-    def check_page_title(self):
-        self.assert_element_text(self.driver, self.title_of_box_xpath, self.header_of_box)
+    def title_of_polish_page(self):
+        self.wait_for_element_to_be_clickable(self.sign_in_button_xpath)
+        assert self.get_page_title(self.polish_login_url) == self.expected_polish_title
 
-    password_field_xpath = 
-    sign_in_button_xpath = 
+    def check_header_of_box(self):
+        self.assert_element_text(self.driver, self.header_of_box_xpath, self.expected_header_of_box)
 
-    def type_in_email(self, email):
-        self.field_send_keys(self.login_field_xpath, email)
- main
+    def click_english_language_listbox(self):
+        self.click_on_the_element(self.english_language_listbox_xpath)
+
+    def click_polski_language_option(self):
+        self.click_on_the_element(self.polski_option_xpath)
+
--- test_cases/login_to_the_system.py
+++ test_cases/login_to_the_system.py
@@ -1,38 +1,27 @@
-import os
-import time
 import unittest
-from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.chrome.service import Service as ChromeService
+from webdriver_manager.chrome import ChromeDriverManager
+from pages.login_page import LoginPage
 from selenium import webdriver
 
-from pages.dashboard import Dashboard
-
-from utils.settings import DRIVER_PATH, IMPLICITLY_WAIT
-from pages.login_page import LoginPage
+driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()))
 
 class TestLoginPage(unittest.TestCase):
 
     @classmethod
-    def setUp(self):
-        os.chmod(DRIVER_PATH, 755)
-        self.driver_service = Service(executable_path=DRIVER_PATH)
-        self.driver = webdriver.Chrome(service=self.driver_service)
-        self.driver.get('https://scouts-test.futbolkolektyw.pl/en')
-        self.driver.fullscreen_window()
-        self.driver.implicitly_wait(IMPLICITLY_WAIT)
+    def setUpClass(cls):
+        driver.get('https://scouts.futbolkolektyw.pl/en/')
+        driver.fullscreen_window()
 
     def test_log_in_to_the_system(self):
-        user_login = LoginPage(self.driver)
-        time.sleep(3)
+        user_login = LoginPage(driver)
         user_login.title_of_page()
-        user_login.check_page_title()
-        user_login.type_in_email('user01@getnada.com')
+        user_login.check_header_of_box()
+        user_login.type_in_email('user04@getnada.com')
         user_login.type_in_password('Test-1234')
-        user_login.click_on_the_sign_in_button()
-        time.sleep(15)
-        dashboard_page = Dashboard(self.driver)
-        dashboard_page.title_of_page()
-        time.sleep(5)
+        user_login.click_sign_in_button()
+
 
     @classmethod
-    def tearDown(self):
-        self.driver.quit()
+    def tearDownClass(cls):
+        driver.quit()
